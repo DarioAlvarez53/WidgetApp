ef6237b293127859d58177fd24b29a35bc716787
DarioA52 <darioalvarez@ecohome.com.mx>
2024-07-16 15:18:02 -0600

    go_router_configurado

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:go_router/go_router.dart';
+import 'package:widget_app/presentation/screens/screens.dart';
+
+// GoRouter configuration
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    //las rutas se definen con GoRoute
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+
+    GoRoute(
+      path: '/buttons',
+      builder: (context, state) => const ButtonsScreen(),
+    ),
+
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => const CardsScreen(),
+    ),
+  ],
+);
