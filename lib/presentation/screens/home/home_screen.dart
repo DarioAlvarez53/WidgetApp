84d5e779f58cc1e1795aec61feedd9e3df1320f1
DarioA52 <darioalvarez@ecohome.com.mx>
2024-07-16 15:51:42 -0600

    rutas con nombre implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widget_app/config/menu/menu_items.dart';
+import 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  static const String name = 'home_screen';
+  
   const HomeScreen({super.key});
 
   @override
@@ -63,6 +67,7 @@ class _CustomListTitle extends StatelessWidget {
         // Navigator.pushNamed(context, menuItem.link);
 
         context.push(menuItem.link);
+        // context.pushNamed(CardsScreen.name);
       },
     );
   }
